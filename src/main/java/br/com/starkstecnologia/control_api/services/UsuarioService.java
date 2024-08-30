f5d246c8540b19bd1c11a79d422bae7d7096b4e2
Murilo Henrique <muhenrique.dev@gmail.com>
2024-08-29 21:39:39 -0300

    Ajuste anotação

--- src/main/java/br/com/starkstecnologia/control_api/services/UsuarioService.java
+++ src/main/java/br/com/starkstecnologia/control_api/services/UsuarioService.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
